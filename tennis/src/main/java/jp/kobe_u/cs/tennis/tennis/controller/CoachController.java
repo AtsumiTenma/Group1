2b4e4a99845cb422395657d0fa43a0140a2c63cd
Atsumi <253t202t@gsuite.kobe-u.ac.jp>
2025-07-29 16:48:41 +0900

    日付選択機能の追加

--- tennis/src/main/java/jp/kobe_u/cs/tennis/tennis/controller/CoachController.java
+++ tennis/src/main/java/jp/kobe_u/cs/tennis/tennis/controller/CoachController.java
@@ -4,12 +4,17 @@ import jp.kobe_u.cs.tennis.tennis.entity.Attendance;
 import jp.kobe_u.cs.tennis.tennis.repository.UserRepository;
 import jp.kobe_u.cs.tennis.tennis.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/coach")
@@ -29,9 +34,20 @@ public class CoachController {
     }
 
     @GetMapping("/attendance")
-    public String showAttendanceList(Model model) {
-        List<Attendance> attendances = attendanceService.getRecentAttendancesForCoach(MOCK_COACH_ID);
-        model.addAttribute("attendances", attendances);
-        return "coach_attendance_list"; // templates/coach_attendance_list.html
+    public String showAttendanceList(
+        @RequestParam(value = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+        Model model) {
+
+    List<Attendance> attendances = attendanceService.getRecentAttendancesForCoach(MOCK_COACH_ID);
+
+    if (date != null) {
+        attendances = attendances.stream()
+                .filter(att -> att.getAttendanceDateTime().toLocalDate().isEqual(date))
+                .collect(Collectors.toList());
+        model.addAttribute("selectedDate", date.toString());
     }
+
+    model.addAttribute("attendances", attendances);
+    return "coach_attendance_list";
+}
 }
