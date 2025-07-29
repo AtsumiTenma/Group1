4facf2227521651a3abbb9205007532c8f33e08f
Atsumi <253t202t@gsuite.kobe-u.ac.jp>
2025-07-29 17:49:53 +0900

    コーチ側実装

--- tennis/src/main/java/jp/kobe_u/cs/tennis/tennis/controller/HomeController.java
+++ tennis/src/main/java/jp/kobe_u/cs/tennis/tennis/controller/HomeController.java
@@ -4,18 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jp.kobe_u.cs.tennis.tennis.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
 import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final UserRepository userRepository;
+    private final Long MOCK_STUDENT_ID = 1L;
 
     @GetMapping("/")
     public String home(Model model, Principal principal) {
-        boolean authenticated = (principal != null);
-        String username = authenticated ? principal.getName() : "田中 太郎"; // デフォルトのユーザー名
-
-        model.addAttribute("username", username);
-        model.addAttribute("authenticated", authenticated);
+        userRepository.findById(MOCK_STUDENT_ID)
+                .ifPresent(student -> model.addAttribute("student", student));
         return "home"; // templates/home.html
     }
 }
